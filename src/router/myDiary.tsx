dcbed2c81f136041462b2cf4a13681a5af8022d2
jisooyoon <ygs12567@gmail.com>
2022-04-18 19:06:44 +0900

    Add FeelEmoji and makerpen backgrounds

--- src/router/myDiary.tsx
+++ src/router/myDiary.tsx
@@ -1,3 +1,8 @@
+import { useState } from 'react';
+import styled from 'styled-components';
+import { MainBoard } from '../common/shareStyle';
+import bg_pattern from '../images/bg_pattern.svg';
+
 export interface IData {
   id: {
     id: string;
@@ -14,8 +19,39 @@ export interface IData {
   };
 }
 
+const DiaryToggleBtns = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  button {
+    position: relative;
+    padding: 0.5em;
+    cursor: pointer;
+    background: none;
+    border-radius: 4px;
+    &:hover {
+      background-color: ${(props) => props.theme.lightPinkColor};
+      color: #fcfcfc;
+    }
+  }
+`;
+
 const MyDiary = () => {
-  return null;
+  const [buttonClicked, setButtonClicked] = useState(false);
+  return (
+    <MainBoard bgPattern={bg_pattern}>
+      <DiaryToggleBtns>
+        <button className="diaryList">
+          <span className="diaryButtonBk"></span>
+          <span className="buttonName">Diary List</span>
+        </button>
+        <button className="diaryEdit">
+          <span className="diaryButtonBk"></span>
+          <span className="buttonName">Add Diary</span>
+        </button>
+      </DiaryToggleBtns>
+    </MainBoard>
+  );
 };
 
 export default MyDiary;
